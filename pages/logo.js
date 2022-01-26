d446dd1f58c5e574abcffd6349e688918c6a5bb7
adamewhite <evilofbanality@gmail.com>
2022-01-26 13:19:34 -0500

    logo demo

---
+++ pages/logo.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Image from "next/image";
+import Logo from "../public/martinlogo.png";
+import styled from "styled-components";
+
+const HomeStyles = styled.main`
+  /* width: 500px; */
+  display: flex;
+  justify-content: center;
+
+  div {
+    width: 400px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <HomeStyles>
+      <div>
+        <Image
+          src={Logo}
+          alt="Martin Logo"
+          layout="responsive"
+          placeholder="blur"
+        />
+      </div>
+    </HomeStyles>
+  );
+}
